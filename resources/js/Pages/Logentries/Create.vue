e8965fb8f026bf23eeda3773f26c018fef8f5dc2
n.froggatt <n.froggatt@vehikl.com>
2021-03-05 16:52:35 -0500

    fix quantity and date fields

--- resources/js/Pages/Logentries/Create.vue
+++ resources/js/Pages/Logentries/Create.vue
@@ -1,36 +1,48 @@
 <template>
-<div>
-    <h1>Create Log Entry</h1>
-    <form method="POST" @submit.prevent="submit">
-        <div class="grid grid-cols-2 gap-2">
-            <p class="col-span-2" v-if="errors.quantity">{{errors.quantity}}</p>
-            <label class="p-2" for="quantity">Quantity:</label>
-            <input class="border rounded" id="quantity" type="number" min="0" v-model="quantity">
-            <p class="col-span-2" v-if="errors.consumed_at">{{errors.consumed_at}}</p>
-            <label class="p-2" for="consumed_at">Consumed at:</label>
-            <input class="border rounded" id="consumed_at" type="date" :value="consumed_at">
-        </div>
-    </form>
-</div>
+    <div>
+        <h1>Create Log Entry</h1>
+        <form method="POST" @submit.prevent="submit">
+            <div class="grid grid-cols-2 gap-2">
+                <p class="col-span-2" v-if="errors.quantity">{{errors.quantity}}</p>
+                <label class="p-2" for="quantity">Quantity:</label>
+                <input class="border rounded" id="quantity" type="number" min="0" v-model="logentry.quantity">
+                <p class="col-span-2" v-if="errors.consumed_at">{{errors.consumed_at}}</p>
+                <label class="p-2" for="consumed_at">Consumed at:</label>
+                <input class="border rounded" id="consumed_at_date" type="date" v-model="logentry.consumed_at" @change="update_date">
+            </div>
+        </form>
+        <button @click="store">Save</button>
+        <button>Cancel</button>
+    </div>
 </template>
 
 <script>
 export default {
-    mounted(){
-        this.mydate = new Date();
-        this.foo.setDate(this.foo.getDate(new Date())+10);
-        console.log(this.foo.toISOString().substr(0,10));
-        this.consumed_at=this.foo.toISOString().substr(0,10);
-    },
     props: {
         errors: Object,
+        user: Object
     },
     data() {
         return {
-            foo: 0,
-            food: 'foo',
-            consumed_at: '2021-02-01',
-            quantity: 99
+            logentry: {
+                user_id: this.user.id,
+                food_id: 2,
+                consumed_at: (new Date()).toISOString().substr(0,10),
+                quantity: 0
+            }
+        }
+    },
+    methods: {
+        update_date(){
+            console.log(this.logentry);
+        },
+        store(){
+            this.$inertia.post(
+                this.$route("logentries.store"), this.food
+            ).then(()=>{
+                console.log("errors", this.errors.description);
+            });
         }
     }
 }
+</script>
