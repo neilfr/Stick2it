47059b83e540bd2f6c9d1f86862254e1af8c1a5f
Neil Froggatt <neilfr@live.com>
2021-03-28 13:50:02 -0400

    Feature/12 store a new log entry (#27)
    * fix quantity and date fields
    
    * store default food
    
    * pass foods and foodgroups to logentry create
    
    * add food list to create logentry screen
    
    * add select food and store logentry
    
    Co-authored-by: n.froggatt <n.froggatt@vehikl.com>

--- app/Http/Controllers/LogentryController.php
+++ app/Http/Controllers/LogentryController.php
@@ -2,25 +2,43 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\StoreLogentryRequest;
-use App\Http\Requests\UpdateLogentryRequest;
+use App\Models\Food;
 use Inertia\Inertia;
 use App\Models\Logentry;
+use App\Models\Foodgroup;
 use Illuminate\Http\Request;
+use App\Http\Resources\FoodResource;
+use Illuminate\Support\Facades\Config;
 use App\Http\Resources\LogentryResource;
+use App\Http\Resources\FoodgroupResource;
+use App\Http\Requests\StoreLogentryRequest;
+use App\Http\Requests\UpdateLogentryRequest;
 
 class LogentryController extends Controller
 {
     public function index(Request $request)
     {
         return Inertia::render('Logentries/Index',[
-            'logentries' => LogentryResource::collection(Logentry::where('user_id', auth()->user()->id)->get())
+            'logentries' => LogentryResource::collection(Logentry::where('user_id', auth()->user()->id)->get()),
         ]);
     }
 
-    public function create()
+    public function create(Request $request)
     {
-        return Inertia::render('Logentries/Create');
+        $foodgroups = Foodgroup::all();
+        $foods = Food::userFoods()
+            ->sharedFoods()
+            ->foodgroupSearch($request->query('foodgroupSearch'))
+            ->descriptionSearch($request->query('descriptionSearch'))
+            ->aliasSearch($request->query('aliasSearch'))
+            ->favouritesFilter($request->query('favouritesFilter'))
+            ->with('ingredients')
+            ->paginate(Config::get('ml2.paginator.per_page'));
+
+        return Inertia::render('Logentries/Create',[
+            'foods' => FoodResource::collection($foods),
+            'foodgroups' => FoodgroupResource::collection($foodgroups),
+        ]);
     }
 
     public function store(StoreLogentryRequest $request)
