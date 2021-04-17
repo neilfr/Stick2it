d3a92d83a9859a52ca6c3db26a1112fec7b9ed64
n.froggatt <n.froggatt@vehikl.com>
2021-04-17 15:51:13 -0400

    style logentries index

--- resources/js/Pages/Logentries/Index.vue
+++ resources/js/Pages/Logentries/Index.vue
@@ -1,14 +1,19 @@
 <template>
     <app-layout>
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Log Entries
-            </h2>
+            <div class="flex justify-between">
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                    Log Entries
+                </h2>
+                <button @click="add">
+                    <img class="w-6" src="/images/add-outline.svg">
+                </button>
+            </div>
         </template>
         <div class="container">
             <div class="grid grid-cols-12 gap-2">
-                <div class="col-span-4 grid grid-cols-4 place-items-start border">
-                    <h3 class="col-span-4">Period</h3>
+                <div class="col-span-4 grid grid-cols-4 place-items-start border p-2">
+                    <h3 class="col-span-4 font-bold text-l text-gray-800">Period</h3>
                     <label for="from">From:</label>
                     <input class="col-span-3 mr-16" type="date" name="from" id="from" v-model='from' @change="goToPageOne">
                     <label for="to">To:</label>
@@ -18,8 +23,8 @@
                     <div class="col-span-3"><br/></div>
 
                 </div>
-                <div class="col-span-4 grid grid-cols-2 border">
-                    <h1 class="col-span-2">Totals for Period</h1>
+                <div class="col-span-4 grid grid-cols-2 border p-2">
+                    <h1 class="col-span-2 font-bold text-l text-gray-800">Totals for Period</h1>
                     <label for="totalKcal">Calories</label>
                     <input type="text" name="totalKcal" id="totalKcal" :value="totalKcal" disabled>
 
@@ -35,8 +40,8 @@
                     <label for="totalPotassium">Potassium</label>
                     <input type="text" name="totalPotassium" id="totalPotassium" :value="totalPotassium" disabled>
                 </div>
-                <div class="col-span-4 grid grid-cols-2 border">
-                    <h1 class="col-span-2">Averages for Period</h1>
+                <div class="col-span-4 grid grid-cols-2 border p-2">
+                    <h1 class="col-span-2 font-bold text-l text-gray-800">Averages for Period</h1>
                     <label for="averageKcal">Calories</label>
                     <input type="text" name="averageKcal" id="averageKcal" :value="averageKcal" disabled>
 
@@ -53,40 +58,43 @@
                     <input type="text" name="averagePotassium" id="averagePotassium" :value="averagePotassium" disabled>
                 </div>
             </div>
-
-            <div class='flex justify-between'>
-                <h1>Log Entries</h1>
-                <button @click="add">Add Log Entry</button>
-            </div>
-            <table>
+            <table class="table-fixed w-full mt-16">
                 <tr>
-                    <th>Date / Time</th>
-                    <th>Description</th>
-                    <th>Quantity</th>
-                    <th>KCal</th>
-                    <th>Fat</th>
-                    <th>Protein</th>
-                    <th>Carbohydrate</th>
-                    <th>Potassium</th>
-                    <th>Actions</th>
+                    <th class="w-1/6">Date / Time</th>
+                    <th class="w-1/3">Description</th>
+                    <th class="text-left translate-x-4 transform -rotate-45 origin-bottom-left">Quantity</th>
+                    <th class="text-left translate-x-4 transform -rotate-45 origin-bottom-left">KCal</th>
+                    <th class="text-left translate-x-4 transform -rotate-45 origin-bottom-left">Fat</th>
+                    <th class="text-left translate-x-4 transform -rotate-45 origin-bottom-left">Protein</th>
+                    <th class="text-left translate-x-4 transform -rotate-45 origin-bottom-left">Carbohydrate</th>
+                    <th class="text-left translate-x-4 transform -rotate-45 origin-bottom-left">Potassium</th>
+                    <th class="text-left translate-x-4 transform -rotate-45 origin-bottom-left">Actions</th>
                 </tr>
-                <tr v-for="logentry in logentries.data" :key="logentry.id">
-                    <td>{{logentry.consumed_at}}</td>
-                    <td>{{logentry.description}}</td>
+                <tr v-for="logentry in logentries.data" :key="logentry.id" class="odd:bg-gray-100 leading-8">
+                    <td class="text-center">{{logentry.consumed_at}}</td>
+                    <td class="truncate">{{logentry.description}}</td>
                     <td>{{logentry.quantity}}</td>
                     <td>{{logentry.kcal}}</td>
                     <td>{{logentry.fat}}</td>
                     <td>{{logentry.protein}}</td>
                     <td>{{logentry.carbohydrate}}</td>
                     <td>{{logentry.potassium}}</td>
-                    <td><button @click="edit(logentry)">Edit</button><button @click="destroy(logentry)">Delete</button></td>
+                    <td class="text-center flex justify-between">
+                        <button @click="edit(logentry)">
+                            <img class="w-6" src="/images/edit-pencil.svg">
+                        </button>
+                        <button @click="destroy(logentry)">
+                            <img class="w-6" src="/images/trash.svg">
+                        </button>
+                    </td>
                 </tr>
             </table>
-            <div>
-                <button @click="goToPageOne">First</button>
-                <button @click="previousPage">Previous</button>
-                <button @click="nextPage">Next</button>
-                <button @click="lastPage">Last</button>
+            <div class="grid grid-cols-12 gap-2 mt-2">
+                <button class="col-span-1 border rounded" @click="goToPageOne">First</button>
+                <button class="col-span-1 border rounded" @click="previousPage">Previous</button>
+                <button class="col-span-1 border rounded" @click="nextPage">Next</button>
+                <button class="col-span-1 border rounded" @click="lastPage">Last</button>
+                <p class="col-span-2">Page: {{logentries.meta.current_page}} of {{logentries.meta.last_page}}</p>
             </div>
         </div>
     </app-layout>
